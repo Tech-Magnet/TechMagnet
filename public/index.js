9943501e9a8e104489ef021bb1d1c66dc5b4a886
Luna Nordbergh <luna.nordbergh2@gmail.com>
2024-01-06 01:04:37 +0100

    Added Files 	modified:   .firebase/hosting.cHVibGlj.cache 	modified:   firebase.json 	new file:   public/about_us/index.html 	new file:   public/contact_us/index.html 	new file:   public/img/TM_logo.png 	new file:   public/index.css 	modified:   public/index.html 	modified:   public/index.js 	new file:   public/store/index.html

--- public/index.js
+++ public/index.js
@@ -1,5 +1,4 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js';
-import { getAuth, connectAuthEmulator, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js';
 
 const firebaseApp = initializeApp({
     apiKey: "AIzaSyCXcSxONLNgKtlBO2N7sCoPaNjwvOJRKJw",
@@ -10,21 +9,4 @@ const firebaseApp = initializeApp({
     appId: "1:614054724902:web:6c1ac0d1052f6052862de7",
     measurementId: "G-QPRV2F0T7Y"
 });
-const auth = getAuth(firebaseApp);
-//connectAuthEmulator(auth, "http://localhost:9099");
 
-const loginEmailPassword = async () => {
-    const loginEmail = document.getElementById('txtEmail').value;
-    const loginPassword = document.getElementById('txtPassword').value;
-
-    try {
-        const userCredential = await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
-        console.log(userCredential.user);
-    }catch (error){
-        console.log(error);
-    }
-
-    
-}
-
-document.getElementById('btnLogin').addEventListener("click", loginEmailPassword);
