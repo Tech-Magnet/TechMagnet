cb185f74c796f3b50d440b64e950d4ab2090d3cd
Luna Nordbergh <luna.nordbergh2@gmail.com>
2024-08-31 13:33:50 +0200

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,12 +1,6 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js';
 
 const firebaseApp = initializeApp({
-    apiKey: "AIzaSyCXcSxONLNgKtlBO2N7sCoPaNjwvOJRKJw",
-    authDomain: "techmagnet-de444.firebaseapp.com",
-    projectId: "techmagnet-de444",
-    storageBucket: "techmagnet-de444.appspot.com",
-    messagingSenderId: "614054724902",
-    appId: "1:614054724902:web:6c1ac0d1052f6052862de7",
-    measurementId: "G-QPRV2F0T7Y"
+    /*API KEYS*/
 });
 
